0eb69f9bc07ad02e598e230ec79a962c2b717ea6
Raiden7 <pascu1987@hotmail.com>
2017-07-07 18:46:17 +0200

    Boton a + 10 y boton b -10

--- Ejercicio05-David/Ejercicio05-David.ino
+++ Ejercicio05-David/Ejercicio05-David.ino
@@ -2,6 +2,7 @@
 
 #define PIN_SERVO 9 //servo
 #define PIN_BOTON_A 2 //boton rojo
+#define PIN_BOTON_B 3 //boton amarillo
 
 Servo miservo;
 
@@ -22,6 +23,8 @@ void loop() {
 void detectaFlanco() {
   static boolean anterior_a = digitalRead(PIN_BOTON_A); //Variable local
   boolean estado_a = digitalRead(PIN_BOTON_A);  //Variable local
+  static boolean anterior_b = digitalRead(PIN_BOTON_B); //Variable local
+  boolean estado_b = digitalRead(PIN_BOTON_B);  //Variable local
 
   if (anterior_a != estado_a) {
     if (estado_a == HIGH) {  //flanco ascendente pull-up
@@ -32,5 +35,15 @@ void detectaFlanco() {
       miservo.write(grados);
     }
     anterior_a = estado_a;  //
+  }
+  if (anterior_b != estado_b) {
+    if (estado_b == HIGH) {  //flanco ascendente pull-up
+      if (grados <= 170)
+        grados -= 10;
+      else
+        grados -= 10;
+      miservo.write(grados);
+    }
+    anterior_b = estado_b;  //
   }
 }
