ca396496994479160994ab1b766cfd7516c81aa5
isoriaa <isoria@fttn.es>
2017-07-07 18:35:31 +0200

    Con la función de flanco

--- Ejercicio05-Ivan/Ejercicio05-Ivan.ino
+++ Ejercicio05-Ivan/Ejercicio05-Ivan.ino
@@ -12,23 +12,28 @@ Servo miservo; //clase para usar el servo
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(pin, INPUT); 
+  pinMode(pin, INPUT);
   miservo.attach(PIN_SERVO); //inicializo el servo
   Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (digitalRead(pin) == 1 && flanco == 0) { // si detectamos tension en el pin y es el flanco de subida hacemos lo de abajo
-    flanco = 1;
-    pulsaciones = pulsaciones += 10;
+  if (detectarFlanco(digitalRead(pin))) { // si detectamos tension en el pin y es el flanco de subida hacemos lo de abajo
     miservo.write(pulsaciones);
     Serial.println(pulsaciones);
   }
-  if (digitalRead(pin) == 0) { // si pin deja de estar high ponemos el flanco a 0
-    flanco = 0;
-  }
-  if (pulsaciones == 170){ // cuando lleguemos a 170 lo mandamos de nuevo a 10
+  if (pulsaciones == 170) { // cuando lleguemos a 170 lo mandamos de nuevo a 10
     pulsaciones = 0;
+  }
+}
+boolean detectarFlanco(boolean estado) { // funcion para detectar el cambio de estado del pin
+  if (estado == HIGH && flanco == 0) {
+    flanco = 1;
+    pulsaciones = pulsaciones += 10;
+    return true;
+  }
+  if (estado == LOW) { // si pin deja de estar high ponemos el flanco a 0
+    flanco = 0;
   }
 }
